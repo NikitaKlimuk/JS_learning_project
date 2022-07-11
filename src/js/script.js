d7c8ec6e6c307580cd450084b1b387115eb6d315
NikitaKlim <nikitaklimukwot@gmail.com>
2022-07-11 18:58:32 +0300

    sicle practice, 3 commit

--- src/js/script.js
+++ src/js/script.js
@@ -238,12 +238,14 @@ console.log(result);
 const lines = 5;
 let result = '';
 
-for (let i = 1; i < lines; i++) {
-
-    for (let j = 0; j < i; j++) {
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
         result += "*";
     }
-    result += '\n';
+    result += "\n";
 }
 
 console.log(result);
